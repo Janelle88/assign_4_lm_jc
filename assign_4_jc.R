f072e46c8bde683202376589c6bb45190c0f9ac3
Janelle88 <jnchristensen1788@gmail.com>
2019-11-11 13:23:59 -0800

    imported lobster csv to project

---
+++ assign_4_jc.R
@@ -0,0 +1,9 @@
+# -------
+# Janelle Christensen
+# Lauren Manzo
+# 11/11/19
+# Lobster Abudance Report
+# ESM 206 Assignment 4
+# -------
+
+lob_abundance <- read_csv("lobster_abundance_sbc_lter.csv")
