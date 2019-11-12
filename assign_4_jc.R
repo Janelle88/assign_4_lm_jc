2776ed65812cadafb36822591db3701757d5974b
Janelle88 <jnchristensen1788@gmail.com>
2019-11-11 17:00:39 -0800

    cleaned names, made a basic line graph and set up rmd

--- assign_4_jc.R
+++ assign_4_jc.R
@@ -6,4 +6,20 @@
 # ESM 206 Assignment 4
 # -------
 
-lob_abundance <- read_csv("lobster_abundance_sbc_lter.csv")
+# -------
+# attach packages
+# read in csv
+# -------
+
+library(tidyverse)
+library(janitor)
+
+lobster_df <- read_csv("lobster_abundance_sbc_lter.csv", na = "-99999") %>% 
+  clean_names()
+
+lobster_year <- lobster_df %>% 
+  group_by(site, year) %>% 
+  summarize("total" = sum(count))
+
+ggplot(data = lobster_year, aes(x = year, y = total, color = site)) +
+  geom_line()
