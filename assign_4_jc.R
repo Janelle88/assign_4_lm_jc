ad9885b49e26a9204a67d64d7f6cc4b1be03a248
Janelle88 <jnchristensen1788@gmail.com>
2019-11-15 11:40:54 -0800

    made table and summary statistics

--- assign_4_jc.R
+++ assign_4_jc.R
@@ -14,6 +14,7 @@
 library(tidyverse)
 library(janitor)
 library(directlabels)
+library(kableExtra)
 
 lobster_df <- read_csv("lobster_abundance_sbc_lter.csv", na = "-99999") %>% 
   clean_names()
@@ -27,6 +28,7 @@ lobster_year <- lobster_df %>%
                               ifelse(site == "NAPL", "Naples",
                                      ifelse(site == "CARP", "Carpinteria",
                                             ifelse(site == "AQUE", "Arroyo Quemado", "Mohawk"))))) # this allows me to have a new column with the non-abbreviated site names
+
 
 ggplot(data = lobster_year, aes(x = year, y = total, group = site)) + # want individual lines by site, need group = site
   geom_line(aes(color = MPA), size = 1.2) +
@@ -56,6 +58,8 @@ lobster_size<- read_csv("lobster_abundance_sbc_lter.csv", na = "-99999") %>%
 
 lobster_size$site_name <- factor(lobster_size$site_name , levels=c("Mohawk", "Carpinteria", "Arroyo Quemado", "Naples", "Isla Vista"))
 
+lobster_year_test <- t.test(lobster_year$year, ) # I don't think this shows anything at all
+
 # --------
 # Graph B
 # --------
@@ -64,31 +68,71 @@ lobster_size$site_name <- factor(lobster_size$site_name , levels=c("Mohawk", "Ca
 # scale_x_discrete(site_name1, site_name2, etc.)
 # breaks = c("Non-MPA", "MPA") was in scale_color_manual but removed it
 
+lobster_size<- read_csv("lobster_abundance_sbc_lter.csv", na = "-99999") %>% 
+  clean_names() %>% 
+  filter(year %in% c("2012", "2018")) %>% 
+  group_by(site, year) %>% 
+  select(year, site, size_mm) %>% 
+  mutate("MPA" = ifelse(site == "IVEE", "MPA",
+                        ifelse(site == "NAPL", "MPA", "Non-MPA"))) %>% 
+  mutate("site_name" = ifelse(site == "IVEE", "Isla Vista",
+                              ifelse(site == "NAPL", "Naples",
+                                     ifelse(site == "CARP", "Carpinteria",
+                                            ifelse(site == "AQUE", "Arroyo Quemado", "Mohawk")))))
 
-# ggplot(data = lobster_size, 
-#        aes(y = size_mm, x = site_name)) +
-#    geom_jitter(aes(ifelse("Mohawk", group_by(year), group_by(year))),
-#                show.legend = FALSE,
-#                size = 0.5)  +
-#   # scale_color_manual(breaks = c("2012", "2018"), values = c("steelblue", "steelblue2")) +
-#   # scale_color_manual(values = c("sandybrown", "sandybrown", "sandybrown", "royalblue4","royalblue4")) +
-#   geom_boxplot(aes(fill = as.character(year)), 
-#                alpha = 0.5, 
-#                outlier.color = NA,
-#                show.legend = FALSE) +
-#   scale_fill_grey() +
-#   theme_minimal() +
-#   labs(title = "x",
-#        x = "year", 
-#        y = "size (mm)",
-#        caption = "caption")
+lobster_size$site_name <- factor(lobster_size$site_name , levels=c("Mohawk", "Carpinteria", "Arroyo Quemado", "Naples", "Isla Vista"))
+
+# --------
+# Graph B
+# --------
+
+ggplot(data = lobster_size, 
+       aes(y = size_mm, x = site_name, fill = as.character(year))) +
+  geom_point(position=position_jitterdodge(), alpha=0.2, aes(color = site_name),
+             show.legend = FALSE,
+             size = 1) +
+  scale_color_manual(breaks = c("MPA", "Non-MPA"), values = c("sandybrown", "sandybrown", "sandybrown", "royalblue4","royalblue4")) +
+  geom_boxplot(alpha = 0.75, outlier.color = NA) +
+  scale_fill_grey()+
+  theme_minimal() +
+  labs(title = "x",
+       x = "year", 
+       y = "size (mm)",
+       caption = "caption",
+       color = "year")
+
+# let's visually explore this data and explore the means of the data
+
+ggplot(data = lobster_size, aes(x = size_mm)) +
+  geom_histogram() +
+  facet_wrap(~site)
+
+# means look evenly distributed, but let's do a qq plot to make sure
+
+ggplot(data = lobster_size, aes(sample = size_mm)) +
+  geom_qq() +
+  facet_wrap(~site)
+
+# means look normally distributed, despite a few outliers at carpinteria and IV
+
+# to really see the data, I'm going to make a summary table
+
+lobster_table <- lobster_size %>% 
+  group_by(site_name) %>% 
+  summarize(mean_size = mean(size_mm, na.rm = TRUE),
+            sd_size = sd(size_mm, na.rm = TRUE),
+            lobster_number = n())
+
+# to make the table accessible on an html I will use the kable function
+
+lobster_table %>% 
+  kable(col.names = c("Site Name",
+                      "Mean Lobster Size (cm)",
+                      "Standard Deviation of Lobster Size",
+                      "Number of Lobsters")) %>% 
+  kable_styling(bootstrap_options = "striped",
+                full_width = F,
+                position = "center") %>% 
+  add_header_above(bold = TRUE,line = TRUE, c("Statistics of Lobsters in the Santa Barbara Channel" = 4))
 
 
-# ggplot(data = lobster_year, aes(x = year, y = total, group = site)) +
-#   geom_col()
-
-# geom_text(data = lobster_year %>% filter(year == last(year)), aes(label = site, 
-#                                                                   x = year + 0.5, 
-#                                                                   y = total, 
-#                                                                   color = site)) ugly
-# Move on to next graph
